1b82d8cbb3ba45ba0d69819806e7ad2a5d7384d9
Pireethi <pireethirag@gmail.com>
2026-04-04 13:17:12 +0530

    coursecontent

--- Backend/src/controllers/courseContentController.js
+++ Backend/src/controllers/courseContentController.js
@@ -28,3 +28,47 @@ export const getCourseContent = async (req, res) => {
     res.status(500).json({ message: "Failed to fetch content" });
   }
 };
+
+export const deleteCourseContent = async (req, res) => {
+  try {
+    const { courseId } = req.params;
+
+    const content = await CourseContent.findOneAndDelete({ courseId });
+
+    if (!content) {
+      return res.status(404).json({ message: "Content not found" });
+    }
+
+    res.status(200).json({ message: "Course content deleted successfully" });
+
+  } catch (error) {
+    console.error("Delete content error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const updateCourseContent = async (req, res) => {
+  try {
+    const { courseId } = req.params;
+    const { modules } = req.body;
+
+    const content = await CourseContent.findOneAndUpdate(
+      { courseId },
+      { modules },
+      { new: true }
+    );
+
+    if (!content) {
+      return res.status(404).json({ message: "Content not found" });
+    }
+
+    res.status(200).json({
+      message: "Course content updated successfully",
+      content
+    });
+
+  } catch (error) {
+    console.error("Update content error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
